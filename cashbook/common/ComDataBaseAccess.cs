b0affa5b00a870085759e48acb913d3d8a4b744b
nanairo-develop <nanairo.develop@gmail.com>
2023-05-22 13:05:42 +0900

    ファイル追加

---
+++ cashbook/common/ComDataBaseAccess.cs
@@ -0,0 +1,44 @@
+﻿using MySqlConnector;
+
+namespace cashbook.common
+{
+    internal class ComDataBaseAccess
+    {
+        #region メソッド
+        #region チェック処理
+        /// <summary>
+        /// 重複している場合trueを返す
+        /// </summary>
+        /// <param name="query">select文</param>
+        /// <returns>true:重複あり, false:重複なし</returns>
+        public static bool IsDuplicateOffice(string query)
+        {
+            bool ret = false;
+
+            MySqlConnection conn = new(ComConst.connStr);
+            try
+            {
+                // 接続を開く
+                conn.Open();
+
+                // SQLを実行する
+                using MySqlCommand command = conn.CreateCommand();
+                command.CommandText = query;
+                MySqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    ret = true;
+                }
+                conn.Close();
+            }
+            catch (MySqlException mse)
+            {
+                _ = MessageBox.Show(mse.Message, "データ取得エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return ret;
+        }
+        #endregion チェック処理
+        #endregion メソッド
+    }
+}
---
+++ cashbook/dto/MOfficeDto.cs
@@ -0,0 +1,32 @@
+﻿namespace cashbook.dto
+{
+    internal class MOfficeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? DisplayOrder { get; set; }
+        public string Error { get; set; }
+
+        public MOfficeDto(string name)
+        {
+            Name = name;
+            try
+            {
+                if (Name == string.Empty)
+                {
+                    Name = string.Empty;
+                    ArgumentNullException argumentNullException = new(nameof(Name), "Invalid name");
+                    throw argumentNullException;
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                Error = ex.Message;
+            }
+            finally
+            {
+                Error ??= string.Empty;
+            }
+        }
+    }
+}
